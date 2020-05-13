5352e25c98bdfad1bfcf2e8235d66db94976ffc9
Nicolo <nicolorizzi1996@gmail.com>
2020-05-13 18:37:04 +0200

    INIT VARIABLES

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,7 @@
 /*! unknown exports (runtime-defined) */
 /*! exports [maybe provided (runtime-defined)] [unused] */
 /*! runtime requirements:  */
-console.log('JS RUN');
+
 })();
 
 (() => {
@@ -17,7 +17,7 @@ console.log('JS RUN');
 /*! unknown exports (runtime-defined) */
 /*! exports [maybe provided (runtime-defined)] [unused] */
 /*! runtime requirements:  */
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n  ╷\n2 │     font-family: $Open_Sans;\r\n  │                  ^^^^^^^^^^\n  ╵\n  src\\scss\\Partials\\_common.scss 2:18  @import\n  C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\src\\scss\\main.scss 1:9                            root stylesheet\n    at processResult (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\webpack\\lib\\NormalModule.js:562:19)\n    at C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\webpack\\lib\\NormalModule.js:645:7\n    at jobDone (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\webpack\\lib\\FileSystemInfo.js:736:5)\n    at FileSystemInfo.createSnapshot (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\webpack\\lib\\FileSystemInfo.js:1078:3)\n    at C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\webpack\\lib\\NormalModule.js:637:32\n    at C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:381:11\n    at C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:239:18\n    at context.callback (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:87584:16)\n    at _render_closure1.call$2 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:77359:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:25587:18)\n    at _RootZone.runBinary$3 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:25591:19)\n    at _FutureListener.handleError$1 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:24041:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:24337:40)\n    at Object._Future__propagateToListeners (C:\\Users\\nicol\\OneDrive\\Desktop\\JAVASCRIPT\\CONSEGNA ESERCIZI\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:3497:88)");
+// extracted by mini-css-extract-plugin
 })();
 
 /******/ })()
